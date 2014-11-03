30ae50c9f49be37e98ee6d5e376da8e0b17f78c5
Toshiaki Maki <makingx@gmail.com>
2014-11-04 01:01:24 +0900

    first commit

---
+++ exercise/01-urlshortener/urlshortener/src/main/java/demo/UrlShortener.java
@@ -0,0 +1,66 @@
+package demo;
+
+import com.google.common.hash.Hashing;
+import org.apache.commons.validator.routines.UrlValidator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ConcurrentHashMap;
+
+@EnableAutoConfiguration
+@ComponentScan
+@RestController
+public class UrlShortener {
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortener.class, args);
+    }
+
+    @Value("${urlshorten.url:http://localhost:${server.port:8080}}")
+    String urlShortenUrl;
+
+    final ConcurrentHashMap<String, String> urlMap = new ConcurrentHashMap<>();
+    final UrlValidator urlValidator = new UrlValidator(new String[]{"http", "https"});
+
+    /**
+     * curl -v -X POST http://localhost:8080/http://google.com
+     */
+    @RequestMapping(value = "**", method = RequestMethod.POST)
+    ResponseEntity<String> save(HttpServletRequest req) {
+        // リクエストURLのうちコンテキストパス以降を取得する。
+        String queryParams = (req.getQueryString() != null) ? "?" + req.getQueryString() : "";
+        String url = (req.getRequestURI() + queryParams).substring(1);
+        if (urlValidator.isValid(url)) {
+            String hash = ""/* TODO (1) URLをハッシュ化。ハッシュアルゴリズムには 32-bit murmur3 algorithm を使用する。 */;
+            // ヒント: com.google.common.hash.Hashing.murmur3_32()
+            // TODO (2) urlMapにhashに紐づくURLを追加する。
+            return new ResponseEntity<>(urlShortenUrl + "/" + hash, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * curl -v -X GET http://localhost:8080/58f3ae21
+     */
+    @RequestMapping(value = "{hash}", method = RequestMethod.GET)
+    ResponseEntity<?> get(@PathVariable String hash) {
+        String url = urlMap.get(hash);
+        //String url = redisTemplate.opsForValue().get(hash);
+        // 本当はリダイレクトするのだが、今回はレスポンスボディに入れて200を返す
+        if (url != null) {
+            return new ResponseEntity<>(url, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ exercise/01-urlshortener/urlshortener/src/test/java/demo/UrlShortenerTest.java
@@ -0,0 +1,71 @@
+package demo;
+
+import com.jayway.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = UrlShortener.class)
+@WebAppConfiguration
+@IntegrationTest({"server.port:0"})
+public class UrlShortenerTest {
+    @Value("${local.server.port}")
+    int port;
+
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void testSaveAndGet() throws Exception {
+        given().log().all()
+                .when()
+                .urlEncodingEnabled(false)
+                .post("/http://google.com")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(is("http://localhost:0/58f3ae21"));
+
+        given().log().all()
+                .when()
+                .urlEncodingEnabled(false)
+                .get("/58f3ae21")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(is("http://google.com"));
+    }
+
+    @Test
+    public void testInvalidUrl() throws Exception {
+        given().log().all()
+                .when()
+                .urlEncodingEnabled(false)
+                .post("/hoge")
+                .then()
+                .log().all()
+                .statusCode(400);
+    }
+
+    @Test
+    public void testNotExistHash() throws Exception {
+        given().log().all()
+                .when()
+                .urlEncodingEnabled(false)
+                .get("/hoge")
+                .then()
+                .log().all()
+                .statusCode(404);
+    }
+}
